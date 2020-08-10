c086fd1892720910ec2c73cf1f289952b4803dc5
jojeho <whwogh@hanmail.net>
2020-08-10 19:40:55 +0000

    change emacs

--- second.sh
+++ second.sh
@@ -6,7 +6,7 @@ xrandr --output VGA-1 --mode 1280x1024
 
 
 sudo apt-get install emacs
-
+cp .emacs ~/.emacs
 
 
 ##anacond ¼³Ä¡
@@ -35,7 +35,10 @@ conda install -c anaconda pymongo
 ##mogo restor
 ##https://velog.io/@killi8n/Mongodb-%EB%8D%A4%ED%94%84%ED%95%98%EA%B3%A0-%EB%B0%B1%EC%97%85%ED%95%98%EA%B8%B0-gejmop2baz
 ##mongorestore --host 127.0.0.1 --port 27017 -u°èÁ¤¸í -pºñ¹ø --drop ¿ø·¡µðºñ¿¡¼­ µå¶ø½ÃÅ³ µðºñ¸í --db ¼±ÅÃÀûÀ¸·Î º¹±¸ÇÏ·Á´Â db¸í "º¹±¸ÇÏ·Á´Â ´ýÇÁµÈ µð·ºÅÍ¸® À§Ä¡"
+
+cd ~/Downloads/
 mongorestore dump
+
 
 
 
