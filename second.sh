a9915dde70896d83f928605e1355443430209df0
jojeho <whwogh@hanmail.net>
2020-08-11 18:45:43 +0900

    Update second.sh

--- second.sh
+++ second.sh
@@ -16,6 +16,7 @@ cp .emacs ~/.emacs
 wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
 bash --yes  Anaconda3-2020.07-Linux-x86_64.sh
 
+exportPATH=~/anaconda3/bin:$PATH
 
 ###conda install -c conda-forge pystan »õ·Î¿î ÄÜ¼ÖÃ¢¿¡¼­ ½ÇÇàÇØ¾ß µÊ
 conda install -y -c conda-forge/label/gcc7 pystan
