578efb82c1d5566c45329cc5df04dec82c8d6acc
jojeho <whwogh@hanmail.net>
2020-08-11 04:58:29 +0000

    a

--- all.sh
+++ all.sh
@@ -53,16 +53,13 @@ conda install -y -c anaconda pymongo
 
 sudo apt-get install -y unzip
 
-
-##mogo restor
-##https://velog.io/@killi8n/Mongodb-%EB%8D%A4%ED%94%84%ED%95%98%EA%B3%A0-%EB%B0%B1%EC%97%85%ED%95%98%EA%B8%B0-gejmop2baz
-##mongorestore --host 127.0.0.1 --port 27017 -u°èÁ¤¸í -pºñ¹ø --drop ¿ø·¡µðºñ¿¡¼­ µå¶ø½ÃÅ³ µðºñ¸í --db ¼±ÅÃÀûÀ¸·Î º¹±¸ÇÏ·Á´Â db¸í "º¹±¸ÇÏ·Á´Â ´ýÇÁµÈ µð·ºÅÍ¸® À§Ä¡"
-
-cd ~/Downloads/
-unzip dump
-mongorestore dump
+cd ~/repo
+git clone https://github.com/jojeho/vs.git
 
 reboot
+
+
+
 
 
 
---
+++ mdb.sh
@@ -0,0 +1,8 @@
+##mogo restor
+##https://velog.io/@killi8n/Mongodb-%EB%8D%A4%ED%94%84%ED%95%98%EA%B3%A0-%EB%B0%B1%EC%97%85%ED%95%98%EA%B8%B0-gejmop2baz
+##mongorestore --host 127.0.0.1 --port 27017 -u°èÁ¤¸í -pºñ¹ø --drop ¿ø·¡µðºñ¿¡¼­ µå¶ø½ÃÅ³ µðºñ¸í --db ¼±ÅÃÀûÀ¸·Î º¹±¸ÇÏ·Á´Â db¸í "º¹±¸ÇÏ·Á´Â ´ýÇÁµÈ µð·ºÅÍ¸® À§Ä¡"
+
+cd ~/Downloads/
+unzip dump
+mongorestore dump
+
