ec53a394fcd953e0929015048de27b251eddc8ab
jojeho <whwogh@hanmail.net>
2020-08-11 21:17:53 +0900

    Update all.sh

--- all.sh
+++ all.sh
@@ -34,6 +34,7 @@ cp .emacs ~/.emacs
 #cd ~/Downloads
 wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
 bash   Anaconda3-2020.07-Linux-x86_64.sh
+exportPATH=~/anaconda3/bin:$PATH
 
 ###conda install -c conda-forge pystan »õ·Î¿î ÄÜ¼ÖÃ¢¿¡¼­ ½ÇÇàÇØ¾ß µÊ
 conda install -y -c conda-forge/label/gcc7 pystan
