dd17bbce1ea7e1965cbddf49c6971caab5d2e3d0
jojeho <whwogh@hanmail.net>
2020-08-13 00:09:00 +0900

    Update anaconda.sh

--- anaconda.sh
+++ anaconda.sh
@@ -1,13 +1,10 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
 bash   Anaconda3-2020.07-Linux-x86_64.sh
 exportPATH=~/anaconda3/bin:$PATH
-
 conda install  -c conda-forge/label/gcc7 pystan
-
+conda install -y -c anaconda pymongo
+rm Anaconda3-2020.07-Linux-x86_64.sh
 ##gcc for anaconda
 sudo apt update
 sudo apt install -y build-essential
 sudo apt-get install -y manpages-dev
-
-conda install -y -c anaconda pymongo
-rm Anaconda3-2020.07-Linux-x86_64.sh
