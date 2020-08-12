928538b6928012e89d616afc3c092974414db2a2
jojeho <whwogh@hanmail.net>
2020-08-12 23:51:09 +0900

    Update anaconda.sh

--- anaconda.sh
+++ anaconda.sh
@@ -1,6 +1,6 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
 bash   Anaconda3-2020.07-Linux-x86_64.sh
-exportPATH=~/anaconda3/bin:$PATH
+#exportPATH=~/anaconda3/bin:$PATH
 
 conda install  -c conda-forge/label/gcc7 pystan
 
