af012b166323537f96a02d0cda1314aed2beb0e2
jojeho <whwogh@hanmail.net>
2020-08-12 22:34:56 +0900

    Create anaconda.sh

---
+++ anaconda.sh
@@ -0,0 +1,13 @@
+wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
+bash   Anaconda3-2020.07-Linux-x86_64.sh
+exportPATH=~/anaconda3/bin:$PATH
+
+conda install  -c conda-forge/label/gcc7 pystan
+
+##gcc for anaconda
+sudo apt update
+sudo apt install -y build-essential
+sudo apt-get install -y manpages-dev
+
+conda install -y -c anaconda pymongo
+rm Anaconda3-2020.07-Linux-x86_64.sh
