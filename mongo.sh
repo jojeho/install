2c15722ff0da9f2baba747b97f39b03ee30e0bdf
jojeho <whwogh@hanmail.net>
2020-08-12 22:36:37 +0900

    Update mongo.sh

--- mongo.sh
+++ mongo.sh
@@ -1,3 +1,5 @@
+###https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 sudo apt-get update
 sudo  apt-get install -y mongodb-org
