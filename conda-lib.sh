fd6874261e38d3c33ca3b531351c85dfa6ed4038
jojeho <whwogh@hanmail.net>
2022-02-25 21:13:32 +0900

    Create conda-lib.sh

---
+++ conda-lib.sh
@@ -0,0 +1,11 @@
+conda install -c conda-forge cmdstanpy -y
+conda activate cmdstan
+
+conda install -c conda-forge matplotlib -y
+
+conda install -c conda-forge arviz -y
+
+conda install -c conda-forge bokeh -y
+
+
+conda install -c conda-forge pymongo -y
